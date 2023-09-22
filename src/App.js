193b1963a38bb8865445811ab692695bdd681342
ceeygithub <cynthiaekhomwandolor@gmail.com>
2023-09-22 12:12:04 +0100

    added footer links

--- src/App.js
+++ src/App.js
@@ -11,18 +11,19 @@ function App() {
 
 
   return (
-
-    <div className="container my-5 py-5 ">
-
-      <header >
-
-        <Search />
-      </header>
-      <main>
-        <Current />
-        <Hour />
-        <Week />
-      </main>
+    <div>
+      <div className="container my-5 py-5 ">
+        <header >
+          <Search />
+        </header>
+        <main>
+          <Current />
+          <Hour />
+          <Week />
+        </main>
+      </div>
+      <div className="ms-5 ps-5 pb-5 footer"> <a href="https://github.com/ceeygithub/react-weather-app" target="_blank" rel="noreferrer" >Open-source code</a> by <a href="https://wonderful-entremet-4edd92.netlify.app" rel="noreferrer" >Cynthia Ekhoe</a> from <a href="https://shecodes.io/" rel="noreferrer"  >She Codes</a>
+      </div>
     </div>
   );
 }
